1b8c33138278a3cdf81a0db8054ba711dcbbab0e
MineriBhange <67961861+MineriBhange@users.noreply.github.com>
2021-08-01 16:46:30 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,36 +1,71 @@
-#include <avr/io.h>
-#include <util/delay.h>
+/**
+ * @file main.c
+ * @author 260213 (you@domain.com)
+ * @brief Activity_1
+ * @version 0.1
+ * @date 2021-04-24
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 
 
+#include "ledstat.h"
+#include "ReadADC.h"
+#include "OutPWM.h"
+#include"UART.h"
+#include<avr/io.h>
 
-int main()
 
+/**
+ * @brief Initialize all the peripherals
+ * 
+ */
+
+void peripheral_init(void)
 {
-
-
-    // Direction of ports
-    DDRB |= (1 << PB0);
-    DDRD &= ~(1 << PD0);
-    DDRD &= ~(1 << PD4);
-
-    // PULL-UP configuration
-    PORTD |= (1 << PD0);
-    PORTD |= (1 << PD4);
-    while (1)
+    /*Configure LED and Switch pins*/
+    InitLED();
+    /*Configure ADC registers and pins*/
+    InitADC();
+    /*Configure PWM registers and pins*/
+    InitPWM();
+    /*Configure UART serial communication pin*/
+    InitUART(103);
+}
+    
+   
+uint16_t temp;
+char temp_data;
+int main(void)
+{
+    /*uint16_t temp;*/
+    // Initialize peripherals
+    peripheral_init();
+    
+    while(1)
     {
-        // condition to check if both switches are on
-        if (!(PIND&(1<<PD0)) && !(PIND&(1<<PD4)))
+        if(SENSOR_ON) //If switch_1 is ON
         {
-            // set LED
-            PORTB |= (1 << PB0);
-            _delay_ms(200);
+            if(HEAT_ON) //If switch_2 is ON
+            {
+                ledstat(LED_ON);//LED is ON
+                temp=ReadADC(0);
+                temp_data = OutPWM(temp);
+                UARTwrite(temp_data);
+
+            }
+            else
+            {
+                
+                ledstat(LED_OFF);
+            }
         }
         else
         {
-            // set off led
-            PORTB &= ~(1 << PB0);
-            _delay_ms(200);
+            ledstat(LED_OFF);//LED is OFF
+            OCR1A=0;
         }
-
     }
+    return 0;
 }
